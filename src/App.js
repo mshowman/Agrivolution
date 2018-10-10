59b0fed346c807f37cf7454d08a12c401d709d8c
MD Showman <mdshowman@gmail.com>
2018-10-10 18:23:45 -0400

    Frontend Sources and Public HTML

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './agrivolutionLogo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img  src={logo} className="App-logo" alt="logo" />
+          <form>
+            <label>
+              Username:
+              <input type="text" name="username" />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input type="password" name="password" />
+            </label>
+            <br />
+            <input type="button" name="Submit" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;

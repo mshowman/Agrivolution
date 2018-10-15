c34b9138fdff20f90007424bc7063e4501f32100
Matt Showman <mdshowman@gmail.com>
2018-10-15 19:13:39 -0400

    added help button with tooltip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Popup from 'reactjs-popup';
 import logo from './agrivolutionLogo.png';
+import help from './help.png';
 import './App.css';
 
 class App extends Component {
@@ -12,14 +14,21 @@ class App extends Component {
             <label>
               Username:
               <input type="text" name="username" />
+                <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center">
+                  <div>Username must be in the form of a valid email.</div>
+                </Popup>
             </label>
             <br />
             <label>
               Password:
               <input type="password" name="password" />
+                <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center">
+                  <div>Password must have at least one captial, one number, and one symbol.</div>
+                </Popup>
             </label>
             <br />
-            <input type="button" name="Submit" value="Submit" />
+            <input type="button" name="SignIn" value="Sign-In" />
+            <input type="button" name="Register" value="Register" />
           </form>
         </header>
       </div>

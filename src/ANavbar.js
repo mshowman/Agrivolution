e74987d1d4e8e5b2f38a31917a9a00e02ff05a44
Nathaniel Lewis <nathaniel.lewis@student.fairfield.edu>
2018-10-20 17:58:24 -0400

    Added hover over for the dropdowns

--- src/ANavbar.js
+++ src/ANavbar.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import {Navbar,Nav,NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
 import logo from './agrivolutionLogo.png';
+import './ANavbar.css';
 
 //change the color of the navbar
 const navbar = {backgroundColor: '#FFFF'};
@@ -20,7 +21,7 @@ class ANavbar extends Component{
         <Navbar.Collapse>
           <Nav >
 
-            <NavDropdown eventKey={3} title="Farms" id="basic-nav-dropdown" onMouseEnter >
+            <NavDropdown eventKey={3} title="Farms" id="basic-nav-dropdown" className="dropdown" >
               <MenuItem eventKey={3.1}>Register</MenuItem>
               <MenuItem eventKey={3.2}>Create Crop Cycle</MenuItem>
               <MenuItem eventKey={3.3}>Business Rules</MenuItem>

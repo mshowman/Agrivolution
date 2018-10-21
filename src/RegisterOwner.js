83defc8a87018fea1d625ab3aa3f71a5f2f565bb
Nathaniel Lewis <nathaniel.lewis@student.fairfield.edu>
2018-10-20 23:58:48 -0400

    Changing around the formatting on the register Owner page.

--- src/RegisterOwner.js
+++ src/RegisterOwner.js
@@ -1,83 +1,88 @@
 //RegisterOwner.js
 
 import React, {Component} from 'react';
-import {FormControl, FormGroup, ControlLabel }from 'react-bootstrap';
+import {FormControl, FormGroup, ControlLabel, Form }from 'react-bootstrap';
 import Popup from 'reactjs-popup';
 import help from './help.png';
-
-const h1Green = {color:'#0080000'};
+import './App.css';
+const h1Green = {};
 
 
 
 class RegisterOwner extends Component {
   render(){
       return(
-      <div className="text-center">
-      <h1 class="h1Green"> Register Owner </h1>
 
-      <div className="text-center">
+        <div>
+          {/*header*/}
+          <div className="text-center">
+            <h1> Register Owner </h1>
 
+          {/*build the form for first name, last name, email, and password*/}
+          <Form inline>
 
-      <form inline>
+            <div class="text-center">
+              <FormGroup controlId="formControlsText">
+                <ControlLabel>First Name:</ControlLabel>
+                <FormControl placeholder="First" type="text"/>
+              </FormGroup>
+            </div>
 
-      <FormGroup controlId="formControlsText">
-        <ControlLabel>First Name:</ControlLabel>
-        <FormControl
-          placeholder="First Name"
-          type="email"
-        />
-        </FormGroup>
+            <div class="text-center">
+              <FormGroup controlId="formControlsText">
+                <ControlLabel>Last Name:</ControlLabel>
+                <FormControl placeholder="Last" type="email"/>
+              </FormGroup>
+            </div>
 
-        <FormGroup controlId="formControlsText">
-          <ControlLabel>Last Name:</ControlLabel>
-          <FormControl
-            placeholder="Last Name"
-            type="email"
-          />
-          </FormGroup>
+            <br/>
 
-        <FormGroup controlId="formControlsEmail">
-          <ControlLabel>Email:</ControlLabel>
-          <FormControl
-            placeholder="Enter Email Address"
-            type="email"
-          />
-        </FormGroup>
+            <div class="text-center">
+              <FormGroup controlId="formControlsText">
+                <ControlLabel>   Phone:</ControlLabel>
+                <FormControl placeholder="098-765-4321" type="tel"/>
+              </FormGroup>
+            </div>
 
-        <FormGroup controlId="formControlsPassword">
-          <ControlLabel>Password:</ControlLabel>
-          <FormControl
-            placeholder="Enter Password"
-            type="password"
-          />
-        </FormGroup>
+            <div class="text-center">
+              <FormGroup controlId="formControlsEmail">
+                <ControlLabel>   Email:</ControlLabel>
+                <FormControl placeholder="Someone@email.com" type="email"/>
+              </FormGroup>
+            </div>
 
-        </form>
+            <br/>
 
+          </Form>
 
-        <form>
-          <label>
-            Username:
-            <input type="text" name="username" />
-              <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center">
-                <div>Username must be in the form of a valid email.</div>
-              </Popup>
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" name="password" />
-              <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center">
-                <div>Password must have at least one captial, one number, and one symbol.</div>
-              </Popup>
-          </label>
-          <br />
-          <br />
-          <input type="button" name="Register" value="Register" />
-        </form>
+          <hr/>
+
+          <form>
+            <label>
+              Username:
+              <input type="text" name="username" />
+                <Popup trigger={<img src={help} alt="help" height='36' width='36' className="App-header"/>} position="right center" on="hover">
+                  <div>Username must be in the form of a valid email.</div>
+                </Popup>
+            </label>
+
+            <br />
+
+            <label>
+              Password:
+              <input type="password" name="password" />
+                <Popup trigger={<img src={help} alt="help" height='36' width='36' className="App-header"/>} position="right center" on="hover">
+                  <div>Password must have at least one captial, one number, and one symbol.</div>
+                </Popup>
+            </label>
+
+            <br />
+            <br />
+            <input type="button" name="Register" value="Register" />
+          </form>
+
+        </div>
       </div>
-      </div>
-
     )
     }
 }

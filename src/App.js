f0b4e0e8e0c1eb866b1f159cd9d12c7634cb121f
Nathaniel Lewis <nathaniel.lewis@student.fairfield.edu>
2018-10-24 18:14:42 -0400

    Updating the forms to be rounded and adding function to ANavbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {FormControl, FormGroup, ControlLabel, Form }from 'react-bootstrap';
 import Popup from 'reactjs-popup';
 import logo from './agrivolutionLogo.png';
 import help from './help.png';
@@ -8,6 +9,7 @@ import './App.css';
 import Dashboard from './Dashboard';
 import ANavbar from './ANavbar.js'
 import RegisterOwner from './RegisterOwner.js';
+import ForgotUserNamePW from './ForgotUserNamePW.js';
 
 
 function login(){
@@ -18,32 +20,47 @@ function register(){
   ReactDOM.render(<div> <ANavbar /> <RegisterOwner /></div>, document.getElementById("root"));
 };
 
+function forgot(){
+  ReactDOM.render(<ForgotUserNamePW />, document.getElementById("root"));
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img  src={logo} className="App-logo" alt="logo" />
-          <form>
-            <label>
-              Username:
-              <input type="text" name="username" />
-                <Popup trigger={<img src={help} alt="help" height="36" width="36" />} position="right center" on="hover">
+
+          <Form inline className="rounded">
+
+          <FormGroup controlId="formControlsText">
+            <ControlLabel>Username:</ControlLabel>
+            <FormControl type="text"/>
+                <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center" on="hover">
                   <div>Username must be in the form of a valid email.</div>
                 </Popup>
-            </label>
+            </FormGroup>
+
             <br />
-            <label>
-              Password:
-              <input type="password" name="password" />
+
+            <FormGroup controlId="formControlsText">
+              <ControlLabel>Password:</ControlLabel>
+              <FormControl type="password"/>
                 <Popup trigger={<img src={help} alt="help" height='36' width='36' />} position="right center" on="hover">
                   <div>Password must have at least one captial, one number, and one symbol.</div>
                 </Popup>
-            </label>
-            <br />
+            </FormGroup>
+
+            <br/>
             <input type="button" name="SignIn" value="Sign-In" onClick={login} />
             <input type="button" name="Register" value="Register" onClick={register} />
-          </form>
+          </Form>
+
+          <br/>
+          <br/>
+
+          <a onClick={forgot}>Forgot username or password?</a>
+
         </header>
       </div>
     );

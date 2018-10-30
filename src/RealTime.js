b9bc081069bd583641bbe7d80f46ba17681b6ecd
Matt Showman <mdshowman@gmail.com>
2018-10-29 20:32:17 -0400

    Added module from react-bootstrap to allow graphing.

--- src/RealTime.js
+++ src/RealTime.js
@@ -1,9 +1,18 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
+import { VictoryChart, VictoryLine } from 'victory';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
-class RealTime extends Component{
+class RealTime extends Component {
 
-    render(){
-        return(
+    constructor(props){
+        super(props);
+        this.state = {
+            hover: false
+        }
+    }
+
+    render() {
+        return (
             <div>
                 <span class="column">
                     <h3><u>Device Status:</u></h3>
@@ -26,12 +35,56 @@ class RealTime extends Component{
 
                 <span class="column">
                     <div>
-                        <p>Farm: [dropdown]</p>
-                        <p>Grow Area:[dropdown]</p>
+                        <p>Farm:     
+                        <DropdownButton
+                                bsSize="medium"
+                                title="[Select Farm]"
+                                id="farmDropdown"
+                                disabled="true"
+                            >
+                                <MenuItem eventKey="1">Farm 1</MenuItem>
+                                <MenuItem eventKey="2">Farm 2</MenuItem>
+                                <MenuItem eventKey="3">Farm 3</MenuItem>
+                                <MenuItem eventKey="4">Farm 4</MenuItem>
+                                <MenuItem eventKey="5">Farm 5</MenuItem>
+                            </DropdownButton>
+                        </p>
+                        <p>Grow Area:  
+                        <DropdownButton
+                                bsSize="medium"
+                                title="[Select Grow Area]"
+                                id="growAreaDropdown"
+                            >
+                                <MenuItem eventKey="1">Grow Area 1</MenuItem>
+                                <MenuItem eventKey="2">Grow Area 2</MenuItem>
+                                <MenuItem eventKey="3">Grow Area 3</MenuItem>
+                                <MenuItem eventKey="4">Grow Area 4</MenuItem>
+                                <MenuItem eventKey="5">Grow Area 5</MenuItem>
+                            </DropdownButton></p>
                     </div>
 
                     <div>
                         <h3><u>Grow Area Status:</u></h3>
+                        <VictoryChart animate={{ duration: 2000 }}>
+                            <VictoryLine
+                                data={[
+                                    { x: 0, y: 68 },
+                                    { x: 1, y: 69 },
+                                    { x: 2, y: 67 },
+                                    { x: 3, y: 68 },
+                                    { x: 4, y: 69 },
+                                    { x: 5, y: 68 },
+                                    { x: 6, y: 69 },
+                                    { x: 7, y: 70 },
+                                    { x: 8, y: 71 },
+                                    { x: 9, y: 66 },
+                                    { x: 10, y: 67 },
+                                    { x: 11, y: 68 },
+                                    { x: 12, y: 69 },
+                                    { x: 13, y: 67 },
+                                    { x: 14, y: 68 }
+                                ]} />
+                        </VictoryChart>
                     </div>
                 </span>
             </div>

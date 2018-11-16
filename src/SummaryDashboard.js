cb001e3113af3013afbc28987690a6e7e18795bd
Nathaniel Lewis <nathaniel.lewis@student.fairfield.edu>
2018-11-16 14:24:46 -0500

    Completed the look and functionality of the Historical Summary Dashboard.

--- src/SummaryDashboard.js
+++ src/SummaryDashboard.js
@@ -1,7 +1,29 @@
 //summary dashboard.js
 
 import React, { Component } from 'react';
-import { DropdownButton, Dropdown,PageHeader, ButtonToolbar,MenuItem,Panel} from 'react-bootstrap';
+import { PageHeader,Panel,Table} from 'react-bootstrap';
+
+var testarray = [
+  {crop:"Tomato",season:"Fall 2018",yield:10,brule:1},
+  {crop:"Lettuce",season:"Fall 2018",yield:50,brule:2},
+  {crop:"Cucumber",season:"Fall 2018",yield:100,brule:3},
+  {crop:"Carrot",season:"Fall 2018",yield:500,brule:4}
+];
+
+
+function currentTable(array){
+
+ return(
+      array.map((v) =>
+          <tr>
+            <td>{v.crop}</td>
+            <td>{v.season}</td>
+            <td>{v.yield}</td>
+            <td>{v.brule}</td>
+          </tr>
+        )
+      );
+}
 
 class SummaryDashboard extends Component{
   constructor(props) {
@@ -10,6 +32,7 @@ class SummaryDashboard extends Component{
       const farms = [];
       const growAreas = [];
       const devices = [];
+      var table = '';
 
       for (let i = 1; i <6 ; i++) {
         farms.push({
@@ -41,11 +64,12 @@ class SummaryDashboard extends Component{
             <PageHeader className="h1"> Historical Summary </PageHeader>
             </div>
 
-          <div>
+          <div className="row">
+
           <div className="col-sm-4"></div>
           <div className="col-sm-4">
 
-
+          {/* This is the section for the Panels of choices*/}
             <Panel>
               <Panel.Title>List of Farms</Panel.Title>
                 <Panel.Body>
@@ -87,14 +111,39 @@ class SummaryDashboard extends Component{
 
               </div>
               <div className="col-sm-4"></div>
+          </div>
+
+
+          <div className="row">
+          <br/>
+
+            <Panel>
+              <Panel.Title>Current</Panel.Title>
+              <Panel.Body>
+              {/*
+                */}
+
+                <Table striped bordered condensed hover>
+                  <thead>
+                    <tr>
+                      <th>Crop</th>
+                      <th>Season</th>
+                      <th>Yield</th>
+                      <th>Business Rule</th>
+                    </tr>
+                  </thead>
+                {currentTable(testarray)}
+                </Table>
+              </Panel.Body>
+            </Panel>
+          </div>
 
           </div>
-          </div>
+
+
 
         );
     }
 }
-
-
 
 export default SummaryDashboard;

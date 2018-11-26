5d4b2c234fbc07f6ad807f6b8da629fef24a8a73
Local Admin <localadmin@Locals-iMac.local>
2018-11-26 12:52:18 -0500

    router enabled

---
+++ src/Main.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard';
+import RegisterFarm from './RegisterFarm';
+import GrowArea from './GrowArea';
+import CropCycle from './CropCycle';
+import RegisterUser from './RegisterUser';
+import RealTime from './RealTime';
+import SummaryDashboard from './SummaryDashboard';
+import AccountProfile from './AccountProfile';
+
+const notMade = () => (
+    <div>
+      <h3>Page not made yet.</h3>
+    </div>
+);
+
+const Main = () => (
+    <main>
+      <Switch>
+        <Route exact path='/' component={Dashboard}/>
+        <Route path='/register-farm' component={RegisterFarm}/>
+        <Route path='/create-grow-area' component={GrowArea}/>
+        <Route path='/create-crop-cycle' component={CropCycle}/>
+        <Route path='/create-business-rule' component={notMade}/>
+        <Route path='/register-user' component={RegisterUser}/>
+        <Route path='/register-device' component={notMade}/>
+        <Route path='/configure-device' component={notMade}/>
+        <Route path='/control-device' component={notMade}/>
+        <Route path='/calibrate-device' component={notMade}/>
+        <Route path='/real-time' component={RealTime}/>
+        <Route path='/summary' component={SummaryDashboard}/>
+        <Route path='/profile' component={AccountProfile}/>
+        <Route path='/subscription' component={notMade}/>
+      </Switch>
+    </main>
+  );
+
+  export default Main;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import AWSSignIn from './AWSSignIn';
 import * as serviceWorker from './serviceWorker';
@@ -8,9 +9,12 @@ import Amplify from 'aws-amplify';
 
 import config from './aws-exports';
 
-Amplify.configure(config)   
- 
-ReactDOM.render(<AWSSignIn />, document.getElementById('root'));
+Amplify.configure(config)
+
+ReactDOM.render((
+    <BrowserRouter>
+        <AWSSignIn />
+    </BrowserRouter>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

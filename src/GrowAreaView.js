591a9b77c081948e1df8646d9120429d96d96e00
Matt Showman <mdshowman@gmail.com>
2018-11-30 20:47:11 -0500

    starting the move

--- src/GrowAreaView.js
+++ src/GrowAreaView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 import './App.css';
 
 class GrowAreaView extends React.Component {
@@ -15,14 +16,32 @@ class GrowAreaView extends React.Component {
       width: "100%",
     }
 
+    const farmChoices = [{ value: 1, label: "Farm 1" }, { value: 2, label: "Farm 2" }, { value: 3, label: "Farm 3" }];
+    const growAreaChoices = [{ value: 1, label: "Grow Area 1" }, { value: 2, label: "Grow Area 2" }, { value: 3, label: "Grow Area 3" }];
+
     this.state = {
-      showModal: false,
-      styles
+      showGrowModal: false,
+      showYieldModal: false,
+      styles,
+      farmChoices,
+      growAreaChoices,
     }
   }
 
-  toggle() {
-    this.setState({ showModal: !this.state.showModal });
+  toggle(type) {
+    if (type === "yield") {
+      this.setState({ showYieldModal: !this.state.showYieldModal });
+    } else {
+      this.setState({ showGrowModal: !this.state.showGrowModal });
+    }
+  }
+
+  handleFarmChange = (selectedFarmOption) => {
+    this.setState({ selectedFarmOption });
+  }
+
+  handleGrowAreaChange = (selectedGrowAreaOption) => {
+    this.setState({ selectedGrowAreaOption });
   }
 
   render() {
@@ -30,7 +49,10 @@ class GrowAreaView extends React.Component {
       <div>
         <div>
           <table width="40%" align="center">
-            <caption><Button bsStyle="success" onClick={this.toggle}>{this.props.name}</Button></caption>
+            <caption>
+              <Button bsStyle="success" onClick={this.toggle}>{this.props.name}</Button>
+              <Button onClick={this.toggle("yield")}>Report Yield</Button>
+            </caption>
             <tbody>
               <tr>
                 <th>Device</th>
@@ -52,7 +74,37 @@ class GrowAreaView extends React.Component {
           </table>
         </div>
         <div>
-          <Modal show={this.state.showModal}>
+          <Modal show={this.state.showYieldModal}>
+            <Modal.Header>
+              <Modal.Title>Report Yield for Grow Area</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Select Farm</h4>
+              <Select
+                value={this.state.selectedFarmOption}
+                onChange={this.handleFarmChange}
+                options={this.state.farmChoices}
+                defaulValue='--Select--'
+              />
+              <br />
+              <h4>Select Grow Area</h4>
+              <Select
+                value={this.state.selectedGrowAreaOption}
+                onChange={this.handleGrowAreaChange}
+                options={this.state.growAreaChoices}
+                defaulValue='--Select--'
+              />
+              <br />
+              <h4>Yield:</h4><input type="text" name="yield" /><br />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.toggle}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+        
+        <div>
+          <Modal show={this.state.showGrowModal}>
             <Modal.Header>
               <Modal.Title>{this.props.name}</Modal.Title>
             </Modal.Header>

538f5a0521a81c6418f34024fc21c918f04731c8
Nathaniel Lewis <nathaniel.lewis@student.fairfield.edu>
2018-11-30 20:37:01 -0500

    updated control

--- src/Control.js
+++ src/Control.js
@@ -7,16 +7,16 @@ import Switch from "react-switch";
 
 
 const devices=[
-  {name:"Device 1",type:"temp",temp:10,humidity:12.5,water:2,light:74,label:'Device1'},
-  {name:"Device 2",type:"temp",temp:20,humidity:24.5,water:3,light:75,label:'Device2'},
-  {name:"Device 3",type:"humidity",temp:30,humidity:32.5,water:5,light:76,label:'Device3'},
-  {name:"Device 4",temp:50,humidity:64.5,water:1,light:78,label:'Device4'},
-  {name:"Device 5",temp:100,humidity:89.5,water:2,light:79,label:'Device5'},
-  {name:"Device 6",temp:90,humidity:22.5,water:6,light:80,label:'Device6'},
-  {name:"Device 7",temp:80,humidity:52.5,water:7,light:77,label:'Device7'},
-  {name:"Device 8",temp:70,humidity:16.5,water:8,light:76,label:'Device8'},
-  {name:"Device 9",temp:60,humidity:79.5,water:9,light:75,label:'Device9'},
-  {name:"Device 10",temp:60,humidity:79.5,water:9,light:75,label:'Device10'},
+  {value:"Device 1",type:"temp",temp:10,humidity:12.5,water:2,light:74,label:'Device1'},
+  {value:"Device 2",type:"temp",temp:20,humidity:24.5,water:3,light:75,label:'Device2'},
+  {value:"Device 3",type:"humidity",temp:30,humidity:32.5,water:5,light:76,label:'Device3'},
+  {value:"Device 4",temp:50,humidity:64.5,water:1,light:78,label:'Device4'},
+  {value:"Device 5",temp:100,humidity:89.5,water:2,light:79,label:'Device5'},
+  {value:"Device 6",temp:90,humidity:22.5,water:6,light:80,label:'Device6'},
+  {value:"Device 7",temp:80,humidity:52.5,water:7,light:77,label:'Device7'},
+  {value:"Device 8",temp:70,humidity:16.5,water:8,light:76,label:'Device8'},
+  {value:"Device 9",temp:60,humidity:79.5,water:9,light:75,label:'Device9'},
+  {value:"Device 10",temp:60,humidity:79.5,water:9,light:75,label:'Device10'},
 ];
 
 const styles = {
